8d968c2a7d6c816b38a9e560c465271b7429919d
lastnitescurry <brian.dinneen@gmail.com>
2017-03-03 15:50:15 +0000

    Fix Docker Install

--- packer/scripts73/docker.sh
+++ packer/scripts73/docker.sh
@@ -1,9 +1,5 @@
-# NREL vagrant-boxes
-# http://yum.puppetlabs.com/el/6/dependencies/x86_64/
-# Install Puppet
 # https://docs.docker.com/engine/installation/linux/centos/
-
 yum install -y yum-utils
-yum-config-manager --add-repo https://docs.docker.com/engine/installation/linux/repo_files/centos/docker.repo
+yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum makecache fast
-yum -y install docker-engine
+yum -y install docker-ce
