90a5ec0086e90ef50bfa1f24d5e5d69fdfe6be61
lastnitescurry <brian.dinneen@gmail.com>
2017-03-03 15:51:14 +0000

    Packer builds. Vagrant manages virtual machine software

--- packer/scripts73/vagrant.sh
+++ packer/scripts73/vagrant.sh
@@ -16,4 +16,4 @@ chmod 0600 /home/vagrant/.ssh/authorized_keys
 chown -R vagrant /home/vagrant/.ssh
 
 # Customize the message of the day
-echo 'Welcome to your Vagrant-built virtual machine.' > /etc/motd
+echo 'Welcome to your Packer built virtual machine.' > /etc/motd
