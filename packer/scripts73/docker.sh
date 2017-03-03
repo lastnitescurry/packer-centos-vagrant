57bc6963adf8b4ce661a92d001ee4b8617879956
lastnitescurry <brian.dinneen@gmail.com>
2017-03-03 16:33:17 +0000

    Set docker to run on boot and let vagrant run it

--- packer/scripts73/docker.sh
+++ packer/scripts73/docker.sh
@@ -3,3 +3,9 @@ yum install -y yum-utils
 yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
 yum makecache fast
 yum -y install docker-ce
+
+# https://docs.docker.com/engine/installation/linux/linux-postinstall/#configure-docker-to-start-on-boot
+systemctl enable docker
+#systemctl start docker
+groupadd docker
+usermod -aG docker vagrant
